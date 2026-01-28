c9d003fbd31dc2449a01f54c2b5ab9b6809b7699
tanuj8124 <tanujkashyap8124@gmail.com>
2026-01-28 21:35:24 +0530

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,20 +14,13 @@ export const metadata: Metadata = {
   generator: 'v0.app',
   icons: {
     icon: [
+      
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
+        url: '/logo.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo.png',
   },
 }
 
